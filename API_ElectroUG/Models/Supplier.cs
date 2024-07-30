33cb4d6e578d9da8d7b8f5612a2bec538c0aba1d
Angie <ameza@kachin.ec>
2024-07-30 00:25:13 -0500

    Creación de Entidades

---
+++ API_ElectroUG/Models/Supplier.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API_ElectroUG.Models
+{
+    public class Supplier
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string BusinessName { get; set; }
+
+        [Required]
+        public string TradeName { get; set; }
+
+        [Required]
+        public string CompanyName { get; set; }
+
+        [Required]
+        public string Ruc { get; set; }
+
+        [Required]
+        public string CompanyPhone { get; set;}
+
+        [Required]
+        public DateTime DateOfEntry { get; set; }
+
+        public bool IsDisabled { get; set; } = false;
+
+    }
+}
