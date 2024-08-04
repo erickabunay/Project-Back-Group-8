c525fbb3d682598571fd37eeffbaaccf382f5ddc
Angie <ameza@kachin.ec>
2024-08-03 21:37:13 -0500

    Creación de clase que hereda de Exception, para realizar excepciones personalizadas.

---
+++ API_ElectroUG/Exceptions/ApiException.cs
@@ -0,0 +1,15 @@
+﻿namespace API_ElectroUG.Exceptions
+{
+    public class ApiException : Exception
+    {
+        public int StatusCode { get; set; }
+        public string PublicMessage { get; set; }
+
+        public ApiException(string message, int statusCode = 400, string publicMessage = null)
+            : base(message)
+        {
+            StatusCode = statusCode;
+            PublicMessage = publicMessage ?? message;
+        }
+    }
+}
